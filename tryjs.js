89eb0fb8a261bdec5745c91a39239c400915146c
22324234@gmail.com <cat@cats-Mac-mini.local>
2021-01-30 12:49:22 -0800

    github remote file

---
+++ tryjs.js
@@ -0,0 +1,16 @@
+// Load text from TextArea                                                
+// Highlight the text and add to div ID.RENDERID                          
+//
+//   Use symbol link in your local java/html code
+//   $b/jslib/aronlib.js
+// 
+//  test it
+//   <head>
+//   <script src="aronlib.js"></script>
+//  </head>
+
+// const myHost = 'http://localhost:8080'
+function mytest1(){
+    alert('github mytest1');
+}
+
